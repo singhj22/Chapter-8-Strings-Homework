7ed56fdcd4d314a94988ac42e068b70a2db10b42
singhj22 <42975589+singhj22@users.noreply.github.com>
2018-10-03 12:45:37 -0400

    Add files via upload

---
+++ Variable_Assignment.py
@@ -0,0 +1,12 @@
+'''
+Created on Oct 3, 2018
+
+@author: Jugat Singh
+Period 5 Python Even Days
+'''
+firstName= input('Please enter your first name: ')
+lastName= input('Please enter your last name: ')
+
+print("\n", firstName, lastName)
+
+
